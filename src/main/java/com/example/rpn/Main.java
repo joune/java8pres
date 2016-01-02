2b2de90130947902c7bccacbf9a6949a5d392ad7
Timur Rubeko <trubeko@kyriba.com>
2016-01-02 21:45:22 +0100

    Initial commit: basic build, main class, test

---
+++ src/main/java/com/example/rpn/Main.java
@@ -0,0 +1,9 @@
+package com.example.rpn;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.println("10 4 3 + 2 * - = -4");
+  }
+}
---
+++ src/test/java/com/example/rpn/RpnTest.java
@@ -0,0 +1,14 @@
+package com.example.rpn;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class RpnTest
+{
+  @Test
+  public void testNothing()
+  {
+    assertFalse("Java 7".equals("functional programming"));
+  }
+}
